1cb89515095757f8ce5b22f2cfbaf7e9824f1019
MrBigBrane <ryanmodafe@gmail.com>
2026-09-07 13:27:39 -0700

    sdf

--- santapp-ruler-dual-parent/src/santapp_ruler/ruler/provenance.py
+++ santapp-ruler-dual-parent/src/santapp_ruler/ruler/provenance.py
@@ -15,7 +15,7 @@ DEFAULT_MODEL_REVISION: Final = "a09a35458c702b33eeacc393d103063234e8bc28"
 #     32768: "SaylorTwift/RULER-32768-Qwen2.5-3B-tokenizer",
 # }
 DATASET_REPOSITORIES: Final[dict[int, str]] = {
-    32768: "MrBigBrane/LongBench-v2-32k-CoT"
+    33000: "MrBigBrane/LongBench-v2-32k-CoT"
 }
 DEFAULT_DATASET_REVISION: Final = "main"
 
