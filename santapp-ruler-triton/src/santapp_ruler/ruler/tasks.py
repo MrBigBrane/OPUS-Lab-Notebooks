69a1354c87fce86103321772bbb6d37df10fe59b
MrBigBrane <ryanmodafe@gmail.com>
2026-09-11 13:22:46 -0700

    sdf

--- santapp-ruler-triton/src/santapp_ruler/ruler/tasks.py
+++ santapp-ruler-triton/src/santapp_ruler/ruler/tasks.py
@@ -59,8 +59,8 @@ TASKS: Final[dict[str, RulerTask]] = {
     "qa_2": RulerTask(
         "qa_2", "qa", 32, "multi-hop question answering over long documents"
     ),
-    "data": RulerTask(
-            "data", "qa", 32, "multiple-choice question answering padded to 32k"
+    "default": RulerTask(
+            "default", "qa", 32, "multiple-choice question answering padded to 32k"
     ),
 }
 
